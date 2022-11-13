ef10eeaa07bfe6a384139e2618b4ce52120e6ad0
Caleb Dean Lampitt <83565528+CalebLampitt@users.noreply.github.com>
2022-11-13 17:51:16 +1300

    45

--- javascript.js
+++ javascript.js
@@ -251,18 +251,3 @@ $(".drag-area" ).droppable({
     });
 });
 
-var location = document.getElementById("location");
-
-						function getLocation() {
-						if (navigator.geolocation) {
-							navigator.geolocation.getCurrentPosition(showPosition);
-						} else { 
-							x.innerHTML = "Geolocation is not supported by this browser.";
-						}
-						}
-
-						function showPosition(position) {
-						x.innerHTML = "Latitude: " + position.coords.latitude + 
-						"<br>Longitude: " + position.coords.longitude;
-						}
-
